abf907193c7cc628b6e1d7537e8c5002292f7797
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-11 17:13:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script >
+export default {
+  //LOGIC HERE...
+};
+</script>
+
+<template>
+  <h1 class="text-center mt-5 ">Hello world!</h1>
+</template>
+
+<style>
+/* --- STYLE HERE... --- */
+
+/*CSS reset*/
+
+/*Pseudo-classes*/
+
+/*Generics*/
+
+/*Utilties*/
+</style>
