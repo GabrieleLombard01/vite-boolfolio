4e14da94edfd340264bc631329adbdd4002b3eaf
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-12 18:23:06 +0200

    RouterLink add

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,7 +9,7 @@ export default {
         <div class="container">
             <nav class="navbar navbar-expand-lg bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Portfolio</a>
+                    <RouterLink class="navbar-brand" to="/">Portfolio</RouterLink>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -17,7 +17,10 @@ export default {
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+                            </li>
+                            <li class="nav-item">
+                                <RouterLink class="nav-link active" aria-current="page" to="/contact">Contact</RouterLink>
                             </li>
                         </ul>
                     </div>
