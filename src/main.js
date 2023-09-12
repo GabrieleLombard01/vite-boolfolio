3444a2fa329bb49d2217bcdab1814248f51b0a65
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-12 17:41:51 +0200

    path router fixed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue';
-import {router} from router;
+import {router} from './router';
 
 const app = createApp(App);
-
-app.mount('#app')
+app.use(router);
+app.mount('#app');
