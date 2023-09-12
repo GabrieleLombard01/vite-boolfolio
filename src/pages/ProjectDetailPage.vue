7b79a8a2de98dc166b1cba286e2290160531c53b
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-12 18:39:14 +0200

    DetailPage add

---
+++ src/pages/ProjectDetailPage.vue
@@ -0,0 +1,21 @@
+<script >
+export default {
+    name: 'ProjectDetail'
+}
+</script>
+
+<template>
+    <h2>Pagina dettaglio post</h2>
+</template>
+
+<style>
+/* --- STYLE HERE... --- */
+
+/*CSS reset*/
+
+/*Pseudo-classes*/
+
+/*Generics*/
+
+/*Utilties*/
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import {createRouter, createWebHistory} from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import ContactPage from '../pages/ContactPage.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
+import ProjectDetailPage from '../pages/ProjectDetailPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
        { path: '/', name: 'home', component: HomePage } ,
        { path: '/contact', name: 'contact', component: ContactPage } ,
+       { path: '/projects/:id', name: 'project-detail', component: ProjectDetailPage } ,
        { path: '/not-found', name: 'not-found', component: NotFoundPage } ,
        { path: '/:pathMatch(.*)*', redirect: '/not-found' } ,
     ]
